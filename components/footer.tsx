e4d2701ce1d43da048bfc73a67214bd0495d7d19
Shriprasanna <shriprasanna32@gmail.com>
2024-05-07 20:27:53 +0530

    Fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,27 +4,32 @@ import React from "react"
 const Footer = () => {
   return (
     <footer
-      className="h-[8vh] flex justify-center md:justify-start items-center text-sm md:px-12
+      className="py-2 md:py-0 md:h-[6vh] w-full flex justify-center flex-col md:flex-row gap-1 items-center text-sm md:px-12
      bg-black/10 dark:bg-white/10 text-black dark:text-white backdrop-blur-lg"
     >
-      Build by&nbsp;
-      <Link
-        href="https://github.com/shrix1"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="underline underline-offset-2"
-      >
-        shrix1
-      </Link>
-      .&nbsp; The source code is available on&nbsp;
-      <Link
-        href="https://github.com/shrix1/seo-checkup"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="underline underline-offset-2"
-      >
-        GitHub
-      </Link>
+      <p>
+        Build by&nbsp;
+        <Link
+          href="https://github.com/shrix1"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline underline-offset-2"
+        >
+          shrix1
+        </Link>
+        .
+      </p>
+      <p>
+        The source code is available on&nbsp;
+        <Link
+          href="https://github.com/shrix1/seo-checkup"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline underline-offset-2"
+        >
+          GitHub
+        </Link>
+      </p>
     </footer>
   )
 }
