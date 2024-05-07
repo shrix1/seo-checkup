08f527a60d9285c2bdd4b3abd8cdd108ce0c0036
Shriprasanna <shriprasanna32@gmail.com>
2024-05-07 22:54:44 +0530

    Fix discord webhook redirect url

--- lib/discord-webhook.ts
+++ lib/discord-webhook.ts
@@ -21,8 +21,8 @@ export async function postDiscordLogs(
           **Device**: ${isMobile() ? "Mobile" : "PC"}
           **CHECK_HERE**:${
             type === "SITEMAP"
-              ? `https://seo-checkup.vercel.app/sitemap?q=${value}`
-              : `https://seo-checkup.vercel.app/metadata?q=${value}`
+              ? `https://seocheckup.vercel.app/sitemap?q=${value}`
+              : `https://seocheckup.vercel.app/metadata?q=${value}`
           }`,
         color: type === "SITEMAP" ? 10181046 : 16776960,
       },
